73a47dc4243c68a1ad6f75bb2f2656b9c1577cad
vladmirkulov13 <vladmirkulov13@mail.ru>
2021-02-19 11:39:23 +0400

    Initial commit

---
+++ venv/main2.py
@@ -0,0 +1,17 @@
+from PIL import Image
+from model import *
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+
+    h = 128
+    w = 128
+    image = Image.new('RGB', (h, w))
+    # image = Image.new('L', (h, w))
+    pixels = image.load()
+    for i in range(h):
+        for j in range(w):
+            # pixels[i, j] = 0
+            # pixels[i, j] = 255
+            pixels[i, j] = (i+j)%256
+    image.save("image.png")
